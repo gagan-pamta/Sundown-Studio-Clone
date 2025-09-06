8e471c788f85989bf3d33760d2695c35d8021bc1
Gagan Pamta <pamtagagan00@gmail.com>
2025-09-06 18:08:20 +0530

    Add Footer to the Webpage

--- script.js
+++ script.js
@@ -9,21 +9,37 @@ window.addEventListener("load", () => {
 
 let elemCont = document.querySelector(".element");
 let fxdImg = document.querySelector(".fixed-img");
+
+
 elemCont.addEventListener("mouseenter",function(){
     fxdImg.style.display = "block"
 })
 elemCont.addEventListener("mouseleave",function(){
     fxdImg.style.display = "none"
 })
-
 let elemS = document.querySelectorAll(".elem")
 elemS.forEach((e)=>{
     e.addEventListener("mouseenter", ()=>{
         let img = e.getAttribute("data-img")
         fxdImg.style.backgroundImage = `url(${img})`
-        
     })
 })
+
+
+fxdImg.addEventListener("mouseenter",function(){
+    fxdImg.style.display = "block"
+})
+fxdImg.addEventListener("mouseleave",function(){
+    fxdImg.style.display = "none"
+})
+fxdImg.addEventListener("mouseenter", ()=>{
+    elemS.forEach((e)=>{
+        let img = e.getAttribute("data-img")
+        fxdImg.style.backgroundImage = `url(${img})`
+    }) 
+    })
+
+
 
 
 
