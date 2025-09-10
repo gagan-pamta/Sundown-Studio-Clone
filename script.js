b98dd5054cd81a6a95459e46f9f03b6648f4328d
Gagan Pamta <pamtagagan00@gmail.com>
2025-09-10 07:23:20 +0530

    Add Swiper to the webpage

--- script.js
+++ script.js
@@ -4,41 +4,45 @@ const scroll = new LocomotiveScroll({
 });
 
 window.addEventListener("load", () => {
-  scroll.update();
+    scroll.update();
 });
 
 let elemCont = document.querySelector(".element");
 let fxdImg = document.querySelector(".fixed-img");
 
 
-elemCont.addEventListener("mouseenter",function(){
+elemCont.addEventListener("mouseenter", function () {
     fxdImg.style.display = "block"
 })
-elemCont.addEventListener("mouseleave",function(){
+elemCont.addEventListener("mouseleave", function () {
     fxdImg.style.display = "none"
 })
 let elemS = document.querySelectorAll(".elem")
-elemS.forEach((e)=>{
-    e.addEventListener("mouseenter", ()=>{
+elemS.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
         let img = e.getAttribute("data-img")
         fxdImg.style.backgroundImage = `url(${img})`
     })
 })
 
 
-fxdImg.addEventListener("mouseenter",function(){
+fxdImg.addEventListener("mouseenter", function () {
     fxdImg.style.display = "block"
 })
-fxdImg.addEventListener("mouseleave",function(){
+fxdImg.addEventListener("mouseleave", function () {
     fxdImg.style.display = "none"
 })
-fxdImg.addEventListener("mouseenter", ()=>{
-    elemS.forEach((e)=>{
+fxdImg.addEventListener("mouseenter", () => {
+    elemS.forEach((e) => {
         let img = e.getAttribute("data-img")
         fxdImg.style.backgroundImage = `url(${img})`
-    }) 
     })
+})
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 50,
+});
 
 
 
